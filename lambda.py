e24989614887fc2775fbb8e1c8e8a040a93a9b90
Boopathy-senseai <boopathys@sense7ai.com>
2025-01-05 08:56:31 +0530

    push

--- lambda.py
+++ lambda.py
@@ -1,1 +1,12 @@
-print("am learning cloud")
+import requests
+
+def handler(event, context):
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    res = {
+        "event": event,
+        "output": response.json(),
+        "context": context
+    }
+    print(res)
+
+    return None
