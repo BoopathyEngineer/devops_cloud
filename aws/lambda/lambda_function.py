7d6a5516dc0c4ff573ff7aae169eaac37f8d689b
Boopathy-senseai <boopathys@sense7ai.com>
2025-01-24 09:44:46 +0530

    changes

--- aws/lambda/lambda_function.py
+++ aws/lambda/lambda_function.py
@@ -2,7 +2,8 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": "Hello from Lambda!",
-        "success ":True
+       
     }
 d=lambda_handler("event","context")
 print(d)
+print("222")
